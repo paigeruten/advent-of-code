477bc02e8272def46be1f736339fd2960be63181
Paige Ruten <paige.ruten@gmail.com>
2023-12-10 02:45:46 -0600

    Day 10 (part 2)

--- 2023/day10/pipe_maze.rb
+++ 2023/day10/pipe_maze.rb
@@ -7,19 +7,62 @@ module PipeMaze
     def initialize(tiles)
       @tiles = tiles
       find_start!
+      infer_start_pipe!
     end
 
-    def pipe_loop_diameter
-      pos1, pos2 = connected_neighbours(start)
-      prev1, prev2 = start, start
-      num_steps = 1
-      while pos1 != pos2
-        pos1, prev1 = connected_neighbours(pos1).find { _1 != prev1 }, pos1
-        pos2, prev2 = connected_neighbours(pos2).find { _1 != prev2 }, pos2
-        num_steps += 1
-      end
+    def enclosed_tiles
+      pipe_loop_tiles = pipe_loop.map { |pos| [pos, self[pos]] }.to_h
 
-      num_steps
+      enclosed = []
+      @tiles.each.with_index do |row, y|
+        # Scan the row, keeping track of how many times we've crossed the pipe
+        # loop. Whenever we hit a ground/junk tile, we know it's inside the pipe
+        # loop if we've crossed the pipe loop an odd number of times.
+        #
+        # Thank you Wikipedia: https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
+        pipes_crossed = 0
+        x = 0
+        while x < row.length
+          pipe_tile = pipe_loop_tiles[[x, y]]
+          if pipe_tile
+            if pipe_tile.vertical?
+              pipes_crossed += 1
+            elsif pipe_tile.corner?
+              x += 1
+              x += 1 until pipe_loop_tiles[[x, y]].corner?
+              end_pipe_tile = pipe_loop_tiles[[x, y]]
+
+              # Corners are an interesting edge case (corner case?). They always
+              # come in pairs, possibly with any number of horizontal pipes
+              # between the pair. If the pair points the same vertical direction
+              # (like L--J or F--7), it counts as 2 pipe crossings (or 0,
+              # depending on how you look at it). If they point different
+              # vertical directions (like L--7 or F--J), then it only counts as
+              # 1 pipe crossing.
+              if pipe_tile.vertical_pipe_directions == end_pipe_tile.vertical_pipe_directions
+                pipes_crossed += 2
+              else
+                pipes_crossed += 1
+              end
+            end
+          elsif pipes_crossed.odd?
+            enclosed << [x, y]
+          end
+          x += 1
+        end
+      end
+      enclosed
+    end
+
+    def pipe_loop
+      path = []
+      pos = start
+      begin
+        last_pos = path.last
+        path << pos
+        pos = connected_neighbours(pos).find { _1 != last_pos }
+      end until pos == start
+      path
     end
 
     def connected_neighbours(pos)
@@ -44,11 +87,40 @@ module PipeMaze
         break [x, y] if x
       end
     end
+
+    def infer_start_pipe!
+      x, y = @start
+
+      connections = %i(north west south east).select do |dir|
+        dir = Direction.new(dir)
+        neighbour = self[dir + [x, y]]
+        neighbour && neighbour.pipe_directions.include?(dir.reverse)
+      end
+
+      @tiles[y][x] = case connections
+      when [:north, :south] then ?|
+      when [:west, :east] then ?-
+      when [:north, :west] then ?J
+      when [:north, :east] then ?L
+      when [:west, :south] then ?7
+      when [:south, :east] then ?F
+      else
+        raise "no possible pipe for start tile"
+      end
+    end
   end
 
   class Tile
     def initialize(char)
       @char = char
+    end
+
+    def vertical?
+      @char == ?|
+    end
+
+    def corner?
+      "LJ7F".include? @char
     end
 
     def pipe_directions
@@ -59,8 +131,11 @@ module PipeMaze
         ?J => %i(north west),
         ?7 => %i(south west),
         ?F => %i(south east),
-        ?S => %i(north west south east),
       }[@char] || []).map { Direction.new(_1) }
+    end
+
+    def vertical_pipe_directions
+      pipe_directions.select(&:vertical?)
     end
   end
 
@@ -79,6 +154,10 @@ module PipeMaze
       x, y = pos
       dx, dy = delta
       [x + dx, y + dy]
+    end
+
+    def vertical?
+      [:north, :south].include? @direction
     end
 
     def delta
@@ -111,10 +190,11 @@ class PipeMazePuzzle < Puzzle
   end
 
   def solve_part_one
-    grid.pipe_loop_diameter
+    grid.pipe_loop.length / 2
   end
 
   def solve_part_two
+    grid.enclosed_tiles.length
   end
 
   def self.parse(input)
@@ -156,14 +236,74 @@ if test?
       assert_equal 6649, PipeMazePuzzle.parse(ACTUAL_INPUT).solve_part_one
     end
 
-    def test_example_input_part_two
-      skip
-      assert_equal ??, PipeMazePuzzle.parse(EXAMPLE_INPUT).solve_part_two
+    def test_simple_example_input_part_two
+      input = <<~EOF
+        ...........
+        .S-------7.
+        .|F-----7|.
+        .||.....||.
+        .||.....||.
+        .|L-7.F-J|.
+        .|..|.|..|.
+        .L--J.L--J.
+        ...........
+      EOF
+
+      assert_equal 4, PipeMazePuzzle.parse(input).solve_part_two
+    end
+
+    def test_squeezed_example_input_part_two
+      input = <<~EOF
+        ..........
+        .S------7.
+        .|F----7|.
+        .||....||.
+        .||....||.
+        .|L-7F-J|.
+        .|..||..|.
+        .L--JL--J.
+        ..........
+      EOF
+
+      assert_equal 4, PipeMazePuzzle.parse(input).solve_part_two
+    end
+
+    def test_larger_example_input_part_two
+      input = <<~EOF
+        .F----7F7F7F7F-7....
+        .|F--7||||||||FJ....
+        .||.FJ||||||||L7....
+        FJL7L7LJLJ||LJ.L-7..
+        L--J.L7...LJS7F-7L7.
+        ....F-J..F7FJ|L7L7L7
+        ....L7.F7||L7|.L7L7|
+        .....|FJLJ|FJ|F7|.LJ
+        ....FJL-7.||.||||...
+        ....L---J.LJ.LJLJ...
+      EOF
+
+      assert_equal 8, PipeMazePuzzle.parse(input).solve_part_two
+    end
+
+    def test_bits_of_junk_example_input_part_two
+      input = <<~EOF
+        FF7FSF7F7F7F7F7F---7
+        L|LJ||||||||||||F--J
+        FL-7LJLJ||||||LJL-77
+        F--JF--7||LJLJ7F7FJ-
+        L---JF-JLJ.||-FJLJJ7
+        |F|F-JF---7F7-L7L|7|
+        |FFJF7L7F-JF7|JL---7
+        7-L-JL7||F7|L7F-7F7|
+        L.L7LFJ|||||FJL7||LJ
+        L7JLJL-JLJLJL--JLJ.L
+      EOF
+
+      assert_equal 10, PipeMazePuzzle.parse(input).solve_part_two
     end
 
     def test_actual_input_part_two
-      skip
-      assert_equal ??, PipeMazePuzzle.parse(ACTUAL_INPUT).solve_part_two
+      assert_equal 601, PipeMazePuzzle.parse(ACTUAL_INPUT).solve_part_two
     end
   end
 end
